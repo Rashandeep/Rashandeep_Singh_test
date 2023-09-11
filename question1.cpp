a4fd176745514f58b3d01a6f594cfd198b17640e
Rashandeep Singh <rashandeepsingh@v1040-wn-rt-a-111-64.campus-dynamic.uwaterloo.ca>
2023-09-11 17:00:58 -0400

    committing first 2 questions

---
+++ question1.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+
+// a function that takes all 4 coordinates as input and return a string regarding whether it overlaps or not
+string checkOverlap(double x1, double x2, double x3, double x4){
+    string ans;
+    // if a user input x1>x2 it will swap the numbers
+    if(x1>x2)
+        swap(x1,x2);
+    // if a user input x3>x4 it will swap the numbers
+    if(x3>x4)
+        swap(x3, x4);
+    
+    // the below condition is used to sort the 2 points.
+    if(x1>x3){
+        swap(x1,x3);
+        swap(x2,x4);
+    }
+        
+    // if x3 lies between x1 and x2 or x4 lies between x1 and x2 then the lines overlap
+    if((x3>x1 && x3<x2) || (x4>x1 && x4<x2))
+        ans = "lines overlap";
+    else
+        ans = "lines donot overlap";
+    return ans;
+} 
+
+// function to test the overlap function. It takes coordinates and expected outcome as input and return whether the test passed or not
+void test(double x1, double x2, double x3, double x4, string expected){
+    string ans = checkOverlap(x1, x2, x3, x4);
+    // if actual ans = expected then return true else false
+    if(ans == expected)
+        cout << "test passed\n";
+    else
+        cout << "test failed\n";
+}
+
+int main() {
+    // declaring pair to get the coordinates (x1,x2) and (x3,x4)
+    pair<double, double> one;
+    pair<double, double> two;
+    
+    cin>> one.first >> one.second;
+    cin>> two.first >> two.second;
+    // if(one.first>two.first)
+    //     swap(one, two);
+    
+    // getting the ans based on the user input
+    string ans = checkOverlap(one.first, one.second, two.first, two.second);
+    cout << ans <<"\n";
+    
+    // running the test cases
+    
+    // this is trival test case
+    test(1,5,6,8, "lines donot overlap");
+    test(1,5,2,6, "lines overlap");
+    
+    // In below 2 tests the value of x1 is greater than x2 and x3>x4
+    test(5,1,6,2, "lines overlap");
+    test(5,1,8,6, "lines donot overlap");
+    
+    // it tests the -ve values of x's
+    test(-1,-4,-6,-2, "lines overlap");
+    
+    // if one of the point is common, it means that lines donot overlap
+    test(0,4,4,8, "lines donot overlap");
+    
+    // here line lies between 1st and 2nd coordinate
+    test(-1,4,0,2, "lines overlap");
+    
+    // if line 1 lies between line 2
+    test(-1,4,-2,6, "lines overlap");
+    
+
+    return 0;
+}
---
+++ question2.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+// function to check if both the versions are equal or not. It takes the 2 strings as input and return the string as an output
+string checkNumbers(string s1, string s2){
+    // below condition is used to check if there is any negetive sign inside string then through error
+    if((s1.find('-')!= string::npos) || (s2.find('-')!= string::npos))
+        return "versions can't be negetive";
+    
+    int l1 = s1.length();
+    int l2 = s2.length();
+    int i = 0;
+    int j = 0;
+    while(i<l1 || j<l2){
+        int n1 = 0;
+        int n2 = 0;
+        while(i<l1 && s1[i]!='.'){
+            n1 = n1*10 + (s1[i] - '0');
+            i++;
+        }
+        while(j<l2 && s2[j]!='.'){
+            n2 = n2*10 + (s2[j] - '0');
+            j++;
+        }
+        if (n1>n2){
+            return "s1 is greater than s2";
+        }
+        else if (n2>n1){
+            return "s2 is greater than s1";
+        }
+        i++;
+        j++;
+    }
+    return "s1 and s2 are equal";
+}
+
+//function to test checkNumbers function. It takes both the string and expected outcome as an input and returns whether test passed or failed
+void test(string s1, string s2, string expected){
+    string ans = checkNumbers(s1, s2);
+    if(ans == expected)
+        cout << "test passed\n";
+    else
+        cout << "test failed\n";
+}
+
+int main() {
+    string s1;
+    string s2;
+    cin >> s1;
+    cin >> s2;
+    
+    // returning ans based on user input
+    string ans = checkNumbers(s1, s2);
+    cout << ans << "\n";
+    
+    // running the test cases
+    test("1.1", "1.2", "s2 is greater than s1");
+    test("1.2", "1.1", "s1 is greater than s2");
+    test("1.001", "1.01", "s1 and s2 are equal");
+    test("1.0.0", "1.0", "s1 and s2 are equal");
+    test("123.1", "123.01", "s1 and s2 are equal");
+    test("0.0.1", "0.0.0001", "s1 and s2 are equal");
+    test("-1.0", "1.0", "versions can't be negetive");
+    
+    
+    
+    return 0;
+}
